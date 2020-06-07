49d4b7b6f624773c379e2b97f64addd849feaab0
Marc Joye <marcjoye90@googlemail.com>
2020-06-07 23:36:35 +0100

    Update input.js

--- input.js
+++ input.js
@@ -3,7 +3,7 @@ import { snakeBody } from './snake.js'
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
 let gb = document.getElementById('game-board')
-let sb = document.getElementsByClassName('snake')[0]
+
 
 window.addEventListener('keydown', e => {
     switch (e.key) {
@@ -27,23 +27,33 @@ window.addEventListener('keydown', e => {
 })
 
 window.addEventListener('touchstart', e => {
+    let sb = document.getElementsByClassName('snake')[0]
     let sbBox = sb.getBoundingClientRect()
+    
     let x = e.changedTouches[0].pageX
     let y = e.changedTouches[0].pageY
-    if (lastInputDirection.x !== 0) {
+
+    if (lastInputDirection.x === 0) {
         if (x < sbBox.x) {
             inputDirection = { x: -1, y: 0 }
+            console.log("x set");
+            
         } else {
             inputDirection = { x: 1, y: 0 }
+            console.log("x set");
         }
-    } else if (lastInputDirection.y !== 0) {
+    } else if (lastInputDirection.y === 0) {
         if (y < sbBox.y) {
             inputDirection = { x: 0, y: -1 }
+            console.log("y set");
         } else {
             inputDirection = { x: 0, y: 1 }
+            console.log("y set");
         }
     } else {
         inputDirection = lastInputDirection
+        console.log("nothing set")
+        
     }
     
     e.preventDefault()
